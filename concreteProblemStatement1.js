2290198a58db526f021234c83794f255841f2128
LukeMcCann <developer-lukemccann@outlook.com>
2021-12-01 20:03:31 +0000

    comments

--- concreteProblemStatement1.js
+++ concreteProblemStatement1.js
@@ -182,8 +182,8 @@ let charCount = (str) => {
 function isAlphaNumeric(char) {
     const code = char.charCodeAt(0);
     if (!(code > 47 && code < 58) &&  // numeric (0-9)
-        !(code > 64 && code < 91) &&
-        !(code > 96 && code < 123)) {
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
             return false;
         }
         return true;
